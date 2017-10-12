65f1e04b6022163a85db9a9ddc8bb99fb58e3250
ith <ith@iths-Mac-mini.local>
2017-10-12 12:24:16 +0545

    Initial Commit

---
+++ SimpleCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleCalculator
+//
+//  Created by ith on 10/12/17.
+//  Copyright © 2017 ith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
